73ccf73fe6cff7b18991757574bb5bc896c95d63
anuj gupta <anuj.gupta1@sterlite.com>
2019-08-22 14:27:08 +0530

    updated values

--- src/test/java/runner/basic.java
+++ src/test/java/runner/basic.java
@@ -3,7 +3,10 @@ package runner;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
+
+import java.util.Collections;
 
 public class basic {
 
@@ -13,12 +16,15 @@ public class basic {
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
-        options.addArguments("start-maximized");
-        options.addArguments("--disable-infobars");
+        options.addArguments("--start-maximized");
         options.addArguments("--disable-extensions");
         options.addArguments("--disable-gpu");
         options.addArguments("--disable-dev-shm-usage");
-        WebDriver driver = new ChromeDriver(options);
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        options.setExperimentalOption("useAutomationExtension", false);
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability(ChromeOptions.CAPABILITY, options);
+        WebDriver driver = new ChromeDriver(dc);
         driver.get("https://google.com");
         Thread.sleep(2000);
         driver.quit();
